45b180575523ebdb5deb85d854c53adc8e05a3d8
y4553r <abougueniyasser@outlook.com>
2020-01-04 16:37:04 +0100

    First commit

---
+++ src/test/java/com/hoxify/hoxify/HoxifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoxify.hoxify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoxifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
