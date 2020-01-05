6fe6c7dbfd5e4ff4c677039665cc94b8eb1fe377
y4553r <abougueniyasser@outlook.com>
2020-01-05 13:30:04 +0100

    Add User entity/controller/repository/service and create user functionality and tests

---
+++ src/main/java/com/hoxify/hoxify/user/User.java
@@ -0,0 +1,23 @@
+package com.hoxify.hoxify.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String username;
+	
+	private String displayName;
+	
+	private String password;
+	
+}
---
+++ src/test/java/com/hoxify/hoxify/UserControllerTest.java
@@ -0,0 +1,79 @@
+package com.hoxify.hoxify;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hoxify.hoxify.shared.GenericResponse;
+import com.hoxify.hoxify.user.User;
+import com.hoxify.hoxify.user.UserRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class UserControllerTest {
+	
+	private static final String API_1_0_USERS = "/api/1.0/users";
+	
+	@Autowired
+	TestRestTemplate testRestTemplate;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Before
+	public void cleanUp() {
+		userRepository.deleteAll();
+	}
+	
+	private User createValidUser() {
+		User user = new User();
+		user.setUsername("test-username");
+		user.setDisplayName("test-display");
+		user.setPassword("P4assword");
+		return user;
+	}
+	
+	@Test
+	public void postUser_whenUserIsValid_receiveOk() {
+		User user = createValidUser();
+		ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	
+	@Test
+	public void postUser_whenUserIsValid_userSavedToDatabase() {
+		User user = createValidUser();
+		testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+		assertThat(userRepository.count()).isEqualTo(1);
+	}
+	
+	@Test
+	public void postUser_whenUserIsValid_receiveSuccessMessage() {
+		User user = createValidUser();
+		ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
+		assertThat(response.getBody().getMessage()).isNotNull();
+	}
+	
+	@Test
+	public void postUser_whenUserIsValid_passwordIsHashedInDatabase() {
+		User user = createValidUser();
+		testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+		List<User> users = userRepository.findAll();
+		User isDB = users.get(0);
+		assertThat(isDB.getPassword()).isNotEqualTo(user.getPassword());
+	}
+}
