e801049c7740c8d7146ea74b397a4f7f46b55738
y4553r <abougueniyasser@outlook.com>
2020-01-09 15:35:37 +0100

    Add Login errors and tests

--- src/main/java/com/hoxify/hoxify/configuration/SecurityConfiguration.java
+++ src/main/java/com/hoxify/hoxify/configuration/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.hoxify.hoxify.configuration;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -12,7 +14,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		
-		http.httpBasic();
+		http.httpBasic().authenticationEntryPoint(new BasicAutehnticationEntryPoint());
 		
 		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/1.0/login").authenticated()
 			.and()
