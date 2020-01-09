97a1775eab838d066082fcbcbfd832ecaf4bf1d1
y4553r <abougueniyasser@outlook.com>
2020-01-09 11:45:44 +0100

    Add login controller core and tests

---
+++ src/main/java/com/hoxify/hoxify/user/LoginController.java
@@ -0,0 +1,14 @@
+package com.hoxify.hoxify.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+	@PostMapping("/api/1.0/login")
+	void handleLogin() {
+		
+	}
+	
+}
